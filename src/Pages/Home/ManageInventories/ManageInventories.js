940fa2386dc761f543c921a2d23de96c7bc0d9e9
shoumikahmed <shoumikahmed103@gmail.com>
2022-05-09 12:35:04 +0600

    stock update btn complete

--- src/Pages/Home/ManageInventories/ManageInventories.js
+++ src/Pages/Home/ManageInventories/ManageInventories.js
@@ -2,25 +2,44 @@ import React, { useEffect, useState } from 'react';
 import ManageInventory from '../ManageInventory/ManageInventory';
 import useReview from '../../../Hooks/useInventory';
 import './ManageInventories.css'
+import { useNavigate } from 'react-router-dom';
 
 const ManageInventories = () => {
     const [review, setReview] = useReview([])
-    // const [manageInventories, setmanageInventories] = useState([])
-    // useEffect(() => {
-    //     fetch('inventories.json')
-    //         .then(res => res.json())
-    //         .then(data => setmanageInventories(data))
-    // }, [])
+    const navigate = useNavigate()
+    const navigateToServiceDetail = id => {
+        navigate(`/stockupdate/${id}`)
+    }
     return (
         <div className='container'>
             <div className="row">
                 <h1 className='manageinventories-title text-warning mb-5 mt-5'><strong>Inventory</strong></h1>
                 <div className='manageinventories-container'>
                     {
-                        review.map(manageinventory => <ManageInventory
-                            key={manageinventory.id}
-                            manageinventory={manageinventory}
-                        ></ManageInventory>)
+                        review.map(manageinventory => <div
+                            key={manageinventory?._id}
+                        >
+                            <div className='wrapper container pb-5'>
+                                <div className="card-box">
+                                    <img className='' src={manageinventory?.img} alt="" />
+                                    <div className="info">
+                                        <h5 className=' text-warning'>{manageinventory?.name}</h5>
+                                        <p className='m-0 mt-2 text-warning'><strong>Price: ${manageinventory?.price}</strong></p>
+                                        <p className='m-0 mb-2'><small>{manageinventory?.description}</small></p>
+                                        <p className='m-0 mb-2'><strong>Suppliername:</strong> {manageinventory?.suppliername}</p>
+                                        <p className='m-0 mb-2'><strong>Quantity:</strong> {manageinventory?.quantity}</p>
+                                        <div className='d-flex justify-content-between'>
+                                            <div>
+                                                <button onClick={() => navigateToServiceDetail(manageinventory?._id)} className='button'>Stock Update</button>
+                                            </div>
+                                            <div>
+                                                <button className='button'>Delete</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>)
                     }
                 </div>
             </div>
--- src/Pages/Home/ManageInventory/ManageInventory.js
+++ src/Pages/Home/ManageInventory/ManageInventory.js
@@ -1,30 +1,37 @@
-import React from 'react';
-import './ManageInventory.css'
+// import React from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import useReview from '../../../Hooks/useInventory';
+// import './ManageInventory.css'
 
-const ManageInventory = ({ manageinventory }) => {
-    const { name, img, description, price, quantity, suppliername } = manageinventory
-    return (
-        <div className='wrapper container pb-5'>
-            <div className="card-box">
-                <img className='' src={img} alt="" />
-                <div className="info">
-                    <h5 className=' text-warning'>{name}</h5>
-                    <p className='m-0 mt-2 text-warning'><strong>Price: ${price}</strong></p>
-                    <p className='m-0 mb-2'><small>{description}</small></p>
-                    <p className='m-0 mb-2'><strong>Suppliername:</strong> {suppliername}</p>
-                    <p className='m-0 mb-2'><strong>Quantity:</strong> {quantity}</p>
-                    <div className='d-flex justify-content-between'>
-                        <div>
-                            <button className='button'>Stock Update</button>
-                        </div>
-                        <div>
-                            <button className='button'>Delete</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-};
+// const ManageInventory = () => {
+//     // const { name, img, description, price, quantity, suppliername } = manageinventory
+//     const [review, setReview] = useReview([])
+//     const navigate = useNavigate()
+//     const navigateToServiceDetail = id => {
+//         navigate(`/stockupdate/${id}`)
+//     }
+//     return (
+//         <div className='wrapper container pb-5'>
+//             <div className="card-box">
+//                 <img className='' src={review?.img} alt="" />
+//                 <div className="info">
+//                     <h5 className=' text-warning'>{review?.name}</h5>
+//                     <p className='m-0 mt-2 text-warning'><strong>Price: ${review?.price}</strong></p>
+//                     <p className='m-0 mb-2'><small>{review?.description}</small></p>
+//                     <p className='m-0 mb-2'><strong>Suppliername:</strong> {review?.suppliername}</p>
+//                     <p className='m-0 mb-2'><strong>Quantity:</strong> {review?.quantity}</p>
+//                     <div className='d-flex justify-content-between'>
+//                         <div>
+//                             <button onClick={() => navigateToServiceDetail(inventory?._id)} className='button'>Stock Update</button>
+//                         </div>
+//                         <div>
+//                             <button className='button'>Delete</button>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
 
-export default ManageInventory;
+// export default ManageInventory;
